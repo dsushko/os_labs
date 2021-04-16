d4c377afb2cf62272506a3d3972ac44f435174e5
dsushko <trolfeisiw@gmail.com>
2021-04-16 18:29:15 +0300

    commit again because something happened

---
+++ lab1_linux/cmake-build-debug/CMakeFiles/lab1_linux.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_linux.dir/main.cpp.o"
+  "lab1_linux"
+  "lab1_linux.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_linux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
